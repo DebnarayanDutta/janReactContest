aeb7dd7808c0aa20d5007b6eb838f3793bf16a92
Debnarayan <debnarayanduttaz663@gmail.com>
2023-01-21 23:29:40 +0530

    first commit

---
+++ src/Components/Loginui.js
@@ -0,0 +1,20 @@
+
+function LoginUi(){
+    return(
+        <div>
+            <div>
+                <h2>Username</h2>
+                <input type="text"/>
+            </div>
+            <div>
+                <h2>Password</h2>
+                <input type="password"/>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+            </div>
+    );
+}
+
+export default LoginUi;
